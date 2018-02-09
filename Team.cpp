d55c772118ef3d268f0ae06a3f9a19175c8ba0df
Patrick Ayer <patrickayer35@users.noreply.github.com>
2018-02-08 23:40:04 -0800

    created Team definitions
    added two new data items to represent a team's standing (standing and gamesBack), and added a function for testing puposes

---
+++ Team.cpp
@@ -0,0 +1,517 @@
+//
+//  Team.cpp
+//  c++_files
+//
+//  Created by Patrick Ayer on 1/26/18.
+//  Copyright © 2018 Patrick Ayer. All rights reserved.
+//
+
+#include "inningsConversions.h"
+#include "Team.h"
+#include <iostream>
+
+Team::Team()
+{
+    teamName = "";
+    ownerName = "";
+    teamId = 0;
+    wins = 0;
+    losses = 0;
+    ties = 0;
+    streak = "";
+    standing = "";
+    gamesBack = 0;
+    pitchersStarted = 0;
+    pitchingLimit = 0;
+    acquisitions = 0;
+    acquisitionsLimit = 0;
+    winner = false;
+    luigi = false;
+    overPitchingLimit = false;
+    pitchingQualify = false;
+    runs = 0;
+    singles = 0;
+    doubles = 0;
+    triples = 0;
+    homeRuns = 0;
+    runsBattedIn = 0;
+    walksBatters = 0;
+    strikeoutsBatters = 0;
+    hitByPitches = 0;
+    stolenBases = 0;
+    caughtStealing = 0;
+    cycles = 0;
+    outfieldAssists = 0;
+    errors = 0;
+    battingPoints = 0;
+    inningsPitched = 0.0;
+    outs = 0;
+    hits = 0;
+    earnedRuns = 0;
+    walksPitchers = 0;
+    hitBatters = 0;
+    strikeoutsPitchers = 0;
+    qualityStarts = 0;
+    noHitters = 0;
+    perfectGames = 0;
+    saves = 0;
+    holds = 0;
+    pitchingPoints = 0;
+    totalBases = 0;
+    yoshiEggs = 0;
+    templeRatio = 0.0;
+    totalPoints = 0;
+    rosterCount = 0;
+}
+
+Team::~Team()
+{
+    for (int i = 0; i < rosterCount; i++)
+    {
+        delete rosters[i];
+    }
+}
+
+void Team::setTeamName(string n)
+{
+    teamName = n;
+}
+
+void Team::setOwnerName(string o)
+{
+    ownerName = o;
+}
+
+void Team::setTeamId(int i)
+{
+    teamId = i;
+}
+
+void Team::setWins(int w)
+{
+    wins = w;
+}
+
+void Team::setLosses(int l)
+{
+    losses = l;
+}
+
+void Team::setTies(int t)
+{
+    ties = t;
+}
+
+void Team::setStreak(string s)
+{
+    streak = s;
+}
+
+void Team::setStanding(string s)
+{
+    standing = s;
+}
+
+void Team::setGamesBack(int g)
+{
+    gamesBack = g;
+}
+
+void Team::setPitchersStarted(int p)
+{
+    pitchersStarted = p;
+}
+
+void Team::setPitchingLimit(int l)
+{
+    pitchingLimit = l;
+}
+
+void Team::setAcquisitions(int a)
+{
+    acquisitions = a;
+}
+
+void Team::setAcquisitionsLimit(int l)
+{
+    acquisitionsLimit = l;
+}
+
+void Team::setWinner(bool w)
+{
+    winner = w;
+}
+
+void Team::appendRoster(Roster* const r)
+{
+    rosters.push_back(r);
+    rosterCount += 1;
+}
+
+void Team::setBattingStats()
+{
+    for (int i = 0; i < rosterCount; i++)
+    {
+        int batterCount = rosters[i]->getBatterCount();
+        for (int j = 0; j < batterCount; j++)
+        {
+            Batter* batter = rosters[i]->getBatters()[j];
+            
+            runs += batter->stats[0];
+            singles += batter->stats[1];
+            doubles += batter->stats[2];
+            triples += batter->stats[3];
+            homeRuns += batter->stats[4];
+            runsBattedIn += batter->stats[5];
+            walksBatters += batter->stats[6];
+            strikeoutsBatters += batter->stats[7];
+            hitByPitches += batter->stats[8];
+            stolenBases += batter->stats[9];
+            caughtStealing += batter->stats[10];
+            cycles += batter->stats[11];
+            outfieldAssists += batter->stats[12];
+            errors += batter->stats[13];
+            battingPoints += batter->stats[14];
+        }
+    }
+}
+
+void Team::setPitchingStats()
+{
+    for (int i = 0; i < rosterCount; i++)
+    {
+        int pitcherCount = rosters[i]->getPitcherCount();
+        for (int j = 0; j < pitcherCount; j++)
+        {
+            Pitcher* pitcher = rosters[i]->getPitchers()[j];
+            
+            outs += pitcher->stats[0];
+            hits += pitcher->stats[1];
+            earnedRuns += pitcher->stats[2];
+            walksPitchers += pitcher->stats[3];
+            hitBatters += pitcher->stats[4];
+            strikeoutsPitchers += pitcher->stats[5];
+            qualityStarts += pitcher->stats[6];
+            noHitters += pitcher->stats[7];
+            perfectGames += pitcher->stats[8];
+            saves += pitcher->stats[9];
+            holds += pitcher->stats[10];
+            pitchingPoints += pitcher->stats[11];
+        }
+    }
+}
+
+void Team::setTotalBases()
+{
+    totalBases = singles + 2*doubles + 3*triples + 4*homeRuns + walksBatters + hitByPitches + stolenBases;
+}
+
+void Team::setInningsPitched()
+{
+    inningsPitched = convertOutsToInnings(outs);
+}
+
+void Team::setYoshiEggs()
+{
+    yoshiEggs = saves + holds;
+}
+
+void Team::setTempleRatio()
+{
+    templeRatio = (double) earnedRuns / (hits + walksPitchers + hitBatters);
+}
+
+void Team::setTotalPoints()
+{
+    totalPoints = battingPoints + pitchingPoints;
+}
+
+void Team::setLuigi()
+{
+    
+}
+
+void Team::setOverPitchingLimit()
+{
+    if (pitchersStarted > pitchingLimit)
+    {
+        overPitchingLimit = true;
+    }
+    else
+    {
+        overPitchingLimit = false;
+    }
+}
+
+void Team::setPitchingQualification()
+{
+    if (pitchersStarted >= pitchingLimit)
+    {
+        pitchingQualify = true;
+    }
+    else
+    {
+        pitchingQualify = false;
+    }
+}
+
+string Team::getTeamName()
+{
+    return teamName;
+}
+
+string Team::getOwnerName()
+{
+    return ownerName;
+}
+
+int Team::getTeamId()
+{
+    return teamId;
+}
+
+int Team::getWins()
+{
+    return wins;
+}
+
+int Team::getLosses()
+{
+    return losses;
+}
+
+int Team::getTies()
+{
+    return ties;
+}
+
+string Team::getStreak()
+{
+    return streak;
+}
+
+int Team::getPitchersStarted()
+{
+    return pitchersStarted;
+}
+
+int Team::getPitchingLimit()
+{
+    return pitchingLimit;
+}
+
+int Team::getAcquisitions()
+{
+    return acquisitions;
+}
+
+int Team::getAcquisitionsLimit()
+{
+    return acquisitionsLimit;
+}
+
+bool Team::isWinner()
+{
+    return winner;
+}
+
+bool Team::isLuigi()
+{
+    return luigi;
+}
+
+bool Team::isOverPitchingLimit()
+{
+    return overPitchingLimit;
+}
+
+bool Team::qualifiesForPitchingAwards()
+{
+    return pitchingQualify;
+}
+
+vector<Roster*> Team::getRosterData()
+{
+    return rosters;
+}
+
+int Team::getRuns()
+{
+    return runs;
+}
+
+int Team::getSingles()
+{
+    return singles;
+}
+
+int Team::getDoubles()
+{
+    return doubles;
+}
+
+int Team::getTriples()
+{
+    return triples;
+}
+
+int Team::getHomeRuns()
+{
+    return homeRuns;
+}
+
+int Team::getRunsBattedIn()
+{
+    return runsBattedIn;
+}
+
+int Team::getWalksBatters()
+{
+    return walksBatters;
+}
+
+int Team::getStrikeoutsBatters()
+{
+    return strikeoutsBatters;
+}
+
+int Team::getHitByPitches()
+{
+    return hitByPitches;
+}
+
+int Team::getStolenBases()
+{
+    return stolenBases;
+}
+
+int Team::getCaughtStealing()
+{
+    return caughtStealing;
+}
+
+int Team::getCycles()
+{
+    return cycles;
+}
+
+int Team::getOutfieldAssists()
+{
+    return outfieldAssists;
+}
+
+int Team::getErrors()
+{
+    return errors;
+}
+
+int Team::getBattingPoints()
+{
+    return battingPoints;
+}
+
+double Team::getInningsPitched()
+{
+    return inningsPitched;
+}
+
+int Team::getOuts()
+{
+    return outs;
+}
+
+int Team::getHits()
+{
+    return hits;
+}
+
+int Team::getEarnedRuns()
+{
+    return earnedRuns;
+}
+
+int Team::getWalksPitchers()
+{
+    return walksPitchers;
+}
+
+int Team::getStrikeoutsPitchers()
+{
+    return strikeoutsPitchers;
+}
+
+int Team::getQualityStarts()
+{
+    return qualityStarts;
+}
+
+int Team::getNoHitters()
+{
+    return noHitters;
+}
+
+int Team::getPerfectGames()
+{
+    return perfectGames;
+}
+
+int Team::getSaves()
+{
+    return saves;
+}
+
+int Team::getHolds()
+{
+    return holds;
+}
+
+int Team::getPitchingPoints()
+{
+    return pitchingPoints;
+}
+
+int Team::getTotalBases()
+{
+    return totalBases;
+}
+
+int Team::getYoshiEggs()
+{
+    return yoshiEggs;
+}
+
+double Team::getTempleRatio()
+{
+    return templeRatio;
+}
+
+int Team::getTotalPoints()
+{
+    return totalPoints;
+}
+
+void Team::printTeamInfo()
+{
+    cout << "Team info for " << teamId << ":\n";
+    cout << "Name: " << teamName << "\n";
+    cout << "Owner: " << ownerName << "\n";
+    cout << "Wins: " << wins << "\n";
+    cout << "Losses: " << losses << "\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Team.h
+++ Team.h
@@ -1,3 +1,11 @@
+//
+//  Team.h
+//  c++_files
+//
+//  Created by Patrick Ayer on 1/26/18.
+//  Copyright © 2018 Patrick Ayer. All rights reserved.
+//
+
 #ifndef Team_h
 #define Team_h
 
@@ -5,180 +13,154 @@
 
 class Team
 {
-
+    
 private:
-	
-	string 	teamName;
-	string 	ownerName;
-	int		teamId;
-	int		wins;
-	int		losses;
-	int		ties;
-	string	streak;
-	int		pitchersStarted;
-	int		pitchingLimit;
-	int		acquisitions;
-	int 	acquisitionsLimit;
-	bool	winner;				// true if team won their matchup
-	bool	luigi;				// true if team is a Luigi (0 roster changes during matchup)
-	bool	overPitchingLimit;	// true if pitchersStarted > pitchingLimit
-	
-	int 	runs;
-	int		singles;
-	int		doubles;
-	int		triples;
-	int		homeRuns;
-	int		runsBattedIn;
-	int		walksBatters;
-	int		strikeoutsBatters;
-	int		hitByPitches;
-	int		stolenBases;
-	int		caughtStealing;
-	int		cycles;
-	int		outfieldAssists;
-	int		errors;
-	int		battingPoints;
-	
-	double	inningsPitched;
-	int		hits;
-	int		earnedRuns;
-	int		walksPitchers;
-	int		hitBatters;
-	int		strikeoutsPitchers;
-	int		qualityStarts;
-	int		noHitters;
-	int		perfectGames;
-	int		saves;
-	int		holds;
-	int		pitchingPoints;
-	
-	int		totalBases;	// = singles + 2*doubles + 3*triples + 4*homeRuns + walksBatters + hitByPitches + stolenBases
-	int		outs;		// innings pitched converted to outs
-	int		yoshiEggs;	// = saves + holds
-	double	templeRatio;	// = earnedRuns / (hits + walksPitchers + hitBatters)
-	int		totalPoints;	// = battingPoints + pitchingPoints
-	
-	vector<Roster*> dailyRosters;
-
+    
+    string 	teamName;
+    string 	ownerName;
+    int		teamId;
+    int		wins;
+    int		losses;
+    int		ties;
+    string	streak;
+    string  standing;
+    int     gamesBack;
+    int		pitchersStarted;
+    int		pitchingLimit;
+    int		acquisitions;
+    int 	acquisitionsLimit;
+    bool	winner;				// true if team won their matchup
+    bool	luigi;				// true if team is a Luigi (0 roster changes during matchup)
+    bool	overPitchingLimit;	// true if pitchersStarted > pitchingLimit
+    bool    pitchingQualify;    // true if pitchersStarted >= pitchingLimit (team will then be eligible for pitching awards)
+    
+    vector<Roster*> rosters;
+    int rosterCount;
+    
+    int 	runs;
+    int		singles;
+    int		doubles;
+    int		triples;
+    int		homeRuns;
+    int		runsBattedIn;
+    int		walksBatters;
+    int		strikeoutsBatters;
+    int		hitByPitches;
+    int		stolenBases;
+    int		caughtStealing;
+    int		cycles;
+    int		outfieldAssists;
+    int		errors;
+    int		battingPoints;
+    
+    double	inningsPitched;
+    int		outs;		// innings pitched converted to outs
+    int		hits;
+    int		earnedRuns;
+    int		walksPitchers;
+    int		hitBatters;
+    int		strikeoutsPitchers;
+    int		qualityStarts;
+    int		noHitters;
+    int		perfectGames;
+    int		saves;
+    int		holds;
+    int		pitchingPoints;
+    
+    int		totalBases;	// = singles + 2*doubles + 3*triples + 4*homeRuns + walksBatters + hitByPitches + stolenBases
+    int		yoshiEggs;	// = saves + holds
+    double	templeRatio;	// = earnedRuns / (hits + walksPitchers + hitBatters)
+    int		totalPoints;	// = battingPoints + pitchingPoints
+    
 public:
-	
-	Team();
-	~Team();
-	
-	void setTeamName(string n);
-	void setOwnerName(string o);
-	void setTeamId(int i);
-	void setWins(int w);
-	void setLosses(int l);
-	void setTies(int t);
-	void setStreak(string s);
-	void setPitchersStarted(int p);
-	void setPitchingLimit(int l);
-	void setAcquisitions(int a);
-	void setAcquisitionsLimit(int l);
-	void setWinner(bool w);
-	void setLuigi(bool l);
-	void setoverPitchingLimit(bool o);
-	
-	void appendRoster(Roster* const r);
-	
-	void setRuns(int r);
-	void setSingles(int s);
-	void setDoubles(int d);
-	void setTriples(int t);
-	void setHomeRuns(int hr);
-	void setRunsBattedIn(int rbi);
-	void setWalksBatters(int w);
-	void setStrikeoutsBatters(int k);
-	void setHitByPitches(int hbp);
-	void setStolenBases(int sb);
-	void setCaughtStealing(int cs);
-	void setCycles(int c);
-	void setOutfieldAssists(int o);
-	void setErrors(int e);
-	void setBattingPoints(int p);
-	
-	void setInningsPitched(double ip);
-	void setHits(int h);
-	void setEarnedRuns(int er);
-	void setWalksPitchers(int w);
-	void setHitBatters(int hb);
-	void setStrikeoutsPitchers(int k);
-	void setQualityStarts(int qs);
-	void setNoHitters(int nh);
-	void setPerfectGames(int pg);
-	void setSaves(int s);
-	void setHolds(int h);
-	void setPitchingPoints(int p);
-	
-	void setTotalBases();
-	void setOuts();
-	void setYoshiEggs();
-	void setTempleRatio();
-	void setTotalPoints();
-	
-	string	getTeamName();
-	string 	getOwnerName();
-	int		getTeamId();
-	int		getWins();
-	int		getLosses();
-	int		getTies();
-	string	getStreak();
-	int		getPitchersStarted();
-	int		getPitchingLimit();
-	int		getAcquisitions();
-	int		getAcquisitionsLimit();
-	bool	isWinner();
-	bool	isLuigi();
-	bool	isOverPitchingLimit();
-	
-	vector<Roster*> getRosterData();
-	
-	int getRuns();
-	int getSingles();
-	int getDoubles();
-	int getTriples();
-	int getHomeRuns();
-	int getRunsBattedIn();
-	int getWalksBatters();
-	int getStrikeoutsBatters();
-	int getHitByPitches();
-	int getStolenBases();
-	int getCaughtStealing();
-	int getCycles();
-	int getOutfieldAssists();
-	int getErrors();
-	int getBattingPoints();
-	
-	double	getInningsPitched();
-	int		getHits();
-	int		getEarnedRuns();
-	int		getWalksPitchers();
-	int		getHitBatters();
-	int		getStrikeoutsPitchers();
-	int		getQualityStarts();
-	int		getNoHitters();
-	int		getPerfectGames();
-	int		getSaves();
-	int		getHolds();
-	int		getPitchingPoints();
-	
-	int 	getTotalBases();
-	int 	getOuts();
-	int 	getYoshiEggs();
-	double 	getTempleRatio();
-	int		getTotalPoints();
-	
+    
+    Team();
+    ~Team();
+    
+    void setTeamName(string n);
+    void setOwnerName(string o);
+    void setTeamId(int i);
+    void setWins(int w);
+    void setLosses(int l);
+    void setTies(int t);
+    void setStreak(string s);
+    void setStanding(string s);
+    void setGamesBack(int g);
+    void setPitchersStarted(int p);
+    void setPitchingLimit(int l);
+    void setAcquisitions(int a);
+    void setAcquisitionsLimit(int l);
+    void setWinner(bool w);
+    
+    void appendRoster(Roster* const r);
+    
+    void setBattingStats();
+    void setPitchingStats();
+    
+    void setTotalBases();
+    void setInningsPitched();
+    void setYoshiEggs();
+    void setTempleRatio();
+    void setTotalPoints();
+    void setLuigi();
+    void setOverPitchingLimit();
+    void setPitchingQualification();
+    
+    string	getTeamName();
+    string 	getOwnerName();
+    int		getTeamId();
+    int		getWins();
+    int		getLosses();
+    int		getTies();
+    string	getStreak();
+    int		getPitchersStarted();
+    int		getPitchingLimit();
+    int		getAcquisitions();
+    int		getAcquisitionsLimit();
+    bool	isWinner();
+    bool	isLuigi();
+    bool	isOverPitchingLimit();
+    bool    qualifiesForPitchingAwards();
+    
+    vector<Roster*> getRosterData();
+    
+    int getRuns();
+    int getSingles();
+    int getDoubles();
+    int getTriples();
+    int getHomeRuns();
+    int getRunsBattedIn();
+    int getWalksBatters();
+    int getStrikeoutsBatters();
+    int getHitByPitches();
+    int getStolenBases();
+    int getCaughtStealing();
+    int getCycles();
+    int getOutfieldAssists();
+    int getErrors();
+    int getBattingPoints();
+    
+    double	getInningsPitched();
+    int 	getOuts();
+    int		getHits();
+    int		getEarnedRuns();
+    int		getWalksPitchers();
+    int		getHitBatters();
+    int		getStrikeoutsPitchers();
+    int		getQualityStarts();
+    int		getNoHitters();
+    int		getPerfectGames();
+    int		getSaves();
+    int		getHolds();
+    int		getPitchingPoints();
+    
+    int 	getTotalBases();
+    int 	getYoshiEggs();
+    double 	getTempleRatio();
+    int		getTotalPoints();
+    
+    void printTeamInfo();
+    
 };
 
 #endif /* Team_h */
-
-
-
-
-
-
-
-
-
-
