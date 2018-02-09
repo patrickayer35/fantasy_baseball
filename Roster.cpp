1805a127b9e319ed405bfb72ce3b8cfaf720d2cc
Patrick Ayer <patrickayer35@users.noreply.github.com>
2018-02-08 23:40:50 -0800

    added a new method for testing purposes

--- Roster.cpp
+++ Roster.cpp
@@ -1,152 +1,187 @@
-#inclde "Roster.h"
+//
+//  Roster.cpp
+//  c++_files
+//
+//  Created by Patrick Ayer on 1/26/18.
+//  Copyright © 2018 Patrick Ayer. All rights reserved.
+//
+
+#include "Roster.h"
 #include <iostream>
 
 Roster::Roster()
 {
-	date = "";
-	batterCount = 0;
-	pitcherCount = 0;
-	benchCount = 0;
-	rosterCount = 0;
+    date = "";
+    batterCount = 0;
+    pitcherCount = 0;
+    benchCount = 0;
+    rosterCount = 0;
 }
 
 Roster::~Roster()
 {
-	for (int i = 0; i < batterCount; i++)
-	{
-		delete batters[i];
-	}
-	for (int i = 0; i < pitcherCount; i++)
-	{
-		delete pitchers[i];
-	}
-	for (int i = 0; i < benchCount; i++)
-	{
-		delete bench[i];
-	}
+    for (int i = 0; i < batterCount; i++)
+    {
+        delete batters[i];
+    }
+    for (int i = 0; i < pitcherCount; i++)
+    {
+        delete pitchers[i];
+    }
+    for (int i = 0; i < benchCount; i++)
+    {
+        delete bench[i];
+    }
 }
 
 void Roster::setDate(string d)
 {
-	date = d;
+    date = d;
 }
 
 void Roster::appendBatter(Batter* const b)
 {
-	batters.push_back(b);
-	batterCount += 1;
+    batters.push_back(b);
+    batterCount += 1;
 }
 
 void Roster::appendPitcher(Pitcher* const p)
 {
-	pitchers.push_back(p);
-	pitcherCount += 1;
+    pitchers.push_back(p);
+    pitcherCount += 1;
 }
 
 void Roster::appendBenchPlayer(BenchPlayer* const p)
 {
-	bench.push_back(p);
-	benchCount += 1;
+    bench.push_back(p);
+    benchCount += 1;
 }
 
 void Roster::setRosterCount()
 {
-	rosterCount = (batterCount + pitcherCount + benchCount);
+    rosterCount = batterCount + pitcherCount + benchCount;
 }
 
 string Roster::getDate()
 {
-	return date;
+    return date;
 }
 
 vector<Batter*> Roster::getBatters()
 {
-	return batters;
+    return batters;
 }
 
 vector<Pitcher*> Roster::getPitchers()
 {
-	return pitchers;
+    return pitchers;
 }
 
 vector<BenchPlayer*> Roster::getBench()
 {
-	return bench;
+    return bench;
 }
 
 int Roster::getBatterCount()
 {
-	return batterCount;
+    return batterCount;
 }
 
 int Roster::getPitcherCount()
 {
-	return pitcherCount;
+    return pitcherCount;
 }
 
 int Roster::getBenchCount()
 {
-	return benchCount;
+    return benchCount;
 }
 
 int Roster::getRosterCount()
 {
-	return rosterCount;
+    return rosterCount;
 }
 
 bool Roster::isSameRoster(Roster* const r)
 {
-	if (rosterCount != r->getRosterCount())
-	{
-		return false;
-	}
-	if (batterCount != r->getBatterCount())
-	{
-		return false;
-	}
-	if (pitcherCount != r->getPitcherCount())
-	{
-		return false;
-	}
-	if (benchCount != r->getBenchCount())
-	{
-		return false;
-	}
-	for (int i = 0; i < batterCount; i++)
-	{
-		vector<Batter*> otherBatters = r->getBatters();
-		for (int i = 0; i < batterCount; i++)
-		{
-			if (batters[i]->playerName != otherBatters[i]->playerName)
-			{
-				return false;
-			}
-		}
-	}
-	for (int i = 0; i < pitcherCount; i++)
-	{
-		vector<Pitcher*> otherPitchers = r->getPitchers();
-		for (int i = 0; i < pitcherCount; i++)
-		{
-			if (pitchers[i]->playerName != otherPitchers[i]->playerName)
-			{
-				return false;
-			}
-		}
-	}
-	for (int i = 0; i < benchCount; i++)
-	{
-		vector<BenchPlayer*> otherBench = r->getBench();
-		for (int i = 0; i < benchCount; i++)
-		{
-			if (bench[i]->playerName != otherBench[i]->playerName)
-			{
-				return false;
-			}
-		}
-	}
-	return true;
+    if (rosterCount != r->getRosterCount())
+    {
+        return false;
+    }
+    if (batterCount != r->getBatterCount())
+    {
+        return false;
+    }
+    if (pitcherCount != r->getPitcherCount())
+    {
+        return false;
+    }
+    if (benchCount != r->getBenchCount())
+    {
+        return false;
+    }
+    for (int i = 0; i < batterCount; i++)
+    {
+        vector<Batter*> otherBatters = r->getBatters();
+        for (int i = 0; i < batterCount; i++)
+        {
+            if (batters[i]->playerName != otherBatters[i]->playerName)
+            {
+                return false;
+            }
+        }
+    }
+    for (int i = 0; i < pitcherCount; i++)
+    {
+        vector<Pitcher*> otherPitchers = r->getPitchers();
+        for (int i = 0; i < pitcherCount; i++)
+        {
+            if (pitchers[i]->playerName != otherPitchers[i]->playerName)
+            {
+                return false;
+            }
+        }
+    }
+    for (int i = 0; i < benchCount; i++)
+    {
+        vector<BenchPlayer*> otherBench = r->getBench();
+        for (int i = 0; i < benchCount; i++)
+        {
+            if (bench[i]->playerName != otherBench[i]->playerName)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
+
+void Roster::printRoster()
+{
+    cout << "\nBATTERS---\n";
+    for (int i = 0; i < batterCount; i++)
+    {
+        cout << "Name: " << batters[i]->playerName << ", Slot: " << batters[i]->slot << " , Stats:";
+        for (int j = 0; j < 15; j++)
+        {
+            cout << " " << batters[i]->stats[j];
+        }
+        cout << "\n";
+    }
+    cout << "\nPITCHERS---\n";
+    for (int i = 0; i < pitcherCount; i++)
+    {
+        cout << "Name: " << pitchers[i]->playerName << ", Slot: " << pitchers[i]->slot << " , Stats:";
+        for (int j = 0; j < 12; j++)
+        {
+            cout << " " << pitchers[i]->stats[j];
+        }
+        cout << "\n";
+    }
+}
+
+
+
 
 
 
--- Roster.h
+++ Roster.h
@@ -1,3 +1,11 @@
+//
+//  Roster.h
+//  c++_files
+//
+//  Created by Patrick Ayer on 1/26/18.
+//  Copyright © 2018 Patrick Ayer. All rights reserved.
+//
+
 #ifndef Roster_h
 #define Roster_h
 
@@ -8,73 +16,65 @@ using namespace std;
 
 struct Batter
 {
-	string playerName;
-	string slot;
-	string teamName;
-	int stats[15];
+    string playerName;
+    string slot;
+    string teamName;
+    int stats[15];
 };
 
 struct Pitcher
 {
-	string playerName;
-	string slot;
-	string teamName;
-	double innings;
-	int stats[12];	// stats[0] will hold innings pitched converted to outs
+    string playerName;
+    string slot;
+    string teamName;
+    double innings;
+    int stats[12];	// stats[0] will hold innings pitched converted to outs
 };
 
 struct BenchPlayer
 {
-	string playerName;
-	string slot;
-	string teamName;
+    string playerName;
+    string slot;
+    string teamName;
 };
 
 class Roster
 {
-	
+    
 private:
-	
-	string date;
-	vector<Batter*> batters;
-	vector<Pitcher*> pitchers;
-	vector<BenchPlayer*> bench;
-	int batterCount;
-	int pitcherCount;
-	int benchCount;
-	int rosterCount;
-	
+    
+    string date;
+    vector<Batter*> batters;
+    vector<Pitcher*> pitchers;
+    vector<BenchPlayer*> bench;
+    int batterCount;
+    int pitcherCount;
+    int benchCount;
+    int rosterCount;
+    
 public:
-	
-	Roster();
-	~Roster();
-	
-	void setDate(string d);
+    
+    Roster();
+    ~Roster();
+    
+    void setDate(string d);
     void appendBatter(Batter* const b);
-	void appendPitcher(Pitcher* const p);
-	void appendBenchPlayer(BenchPlayer* const p);
-	void setRosterCount();
-	
-	string getDate();
-	vector<Batter*> getBatters();
-	vector<Pitcher*> getPitchers();
-	vector<BenchPlayer*> getBench();
-	int getBatterCount();
-	int getPitcherCount();
-	int getBenchCount();
-	int getRosterCount();
-	
-	bool isSameRoster(Day* const d);
-	
+    void appendPitcher(Pitcher* const p);
+    void appendBenchPlayer(BenchPlayer* const p);
+    void setRosterCount();
+    
+    string getDate();
+    vector<Batter*> getBatters();
+    vector<Pitcher*> getPitchers();
+    vector<BenchPlayer*> getBench();
+    int getBatterCount();
+    int getPitcherCount();
+    int getBenchCount();
+    int getRosterCount();
+    
+    bool isSameRoster(Roster* const d);
+    void printRoster();
+    
 };
 
 #endif /* Roster_h */
-
-
-
-
-
-
-
-
-
